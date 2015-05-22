69e3c2af7dd9be52111b5ceca89d2ce01f3a09de
Praful Rana <prafulrana@gmail.com>
2015-05-22 16:38:08 -0700

    Update factory.js

--- client/js/factory.js
+++ client/js/factory.js
@@ -1,22 +1,22 @@
-todoApp.factory('todosFactory', function($http, $httpProvider) {
+todoApp.factory('todosFactory', function($http) {
   var urlBase = 'http://api-staging-runnabledemo.runnableapp.com:3001/api/todos';
-  $httpProvider.defaults.withCredentials = true;
+
   var _todoService = {};
 
   _todoService.getTodos = function() {
-    return $http.get(urlBase);
+    return $http.get(urlBase, {withCredentials: true});
   };
 
   _todoService.saveTodo = function(todo) {
-    return $http.post(urlBase, todo);
+    return $http.post(urlBase, todo, {withCredentials: true});
   };
 
   _todoService.updateTodo = function(todo) {
-    return $http.put(urlBase, todo);
+    return $http.put(urlBase, todo, {withCredentials: true});
   };
 
   _todoService.deleteTodo = function(id) {
-    return $http.delete(urlBase + '/' + id);
+    return $http.delete(urlBase + '/' + id, {withCredentials: true});
   };
 
   return _todoService;
