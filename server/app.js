071534b21f3bb31cf06765e55c1a6795dbd7c93f
Praful Rana <prafulrana@gmail.com>
2015-05-22 14:10:01 -0700

    web v1

---
+++ server/app.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    var express = require('express');
+    var path = require('path');
+    var logger = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser = require('body-parser');
+
+    var routes = require('./routes/index');
+
+    var app = express();
+
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+
+
+    app.use(express.static(path.join(__dirname, '../client')));
+
+
+    app.use('/', routes);
+
+    var server = app.listen(3000, function() {
+        console.log('Express server listening on port ' + server.address().port);
+    });
+
+    module.exports = app;
+}());
