62a678f21836f398050f3dd6b0920e76fd293598
sundippatel <sundip.patel@outlook.com>
2016-01-26 13:29:41 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@
     app.use('/', routes);
 
     var server = app.listen(3000, function() {
+        console.log("Pushing a new change...")
         console.log('Express server listening on port ' + server.address().port);
     });
 
