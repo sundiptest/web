f5cf9dec864e8e31b5db114b87584d8087249a2c
Praful Rana <prafulrana@gmail.com>
2016-02-16 12:29:45 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@
     var logger = require('morgan');
     var cookieParser = require('cookie-parser');
     var bodyParser = require('body-parser');
+    //
 
     var routes = require('./routes/index');
 
